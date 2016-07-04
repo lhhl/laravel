ab7c4b3d712d8e8fc3e75e82d6a4e53b3783581e
lehuuhoanlong <lehuu.hoanlong@gmail.com>
2016-07-04 15:57:20 +0700

    add test.php

--- test.php
+++ test.php
@@ -19,7 +19,43 @@
         curl_close($ch);
         return $result;
 	}
-	header("Content-type: text/xml");
-	echo testCurl();
 
+	function ftpTransfer()
+	{
+		$ftpServer = '192.168.0.229';
+		$ftpUsername = 'root';
+		$ftpPassword = '123456';
+		$ftpConn = ftp_ssl_connect($ftpServer, 21) or die("Could not connect ssl ftp");
+		//$ftpConn = ftp_connect($ftpServer, 21) or die("Could not connect");
+		$login = ftp_login($ftpConn, $ftpUsername, $ftpPassword);
+		ftp_pasv($ftpConn, true);
+		echo "Connect successfully!";
+		$file = "ftpTest.txt";
+		if (ftp_put($ftpConn, "ftpTest.txt", $file, FTP_BINARY)) {
+			echo "Uploaded";
+		} else {
+			echo "Error";
+		}
+		ftp_close($ftpConn);
+		echo "Closed";
+	}
+
+	function sftpTransfer()
+	{
+		$ftpServer = '192.168.0.229';
+		$ftpUsername = 'root';
+		$ftpPassword = '123456';
+
+		$connection = ssh2_connect($ftpServer);
+		ssh2_auth_password($connection, $ftpUsername, $ftpPassword);
+		$sftp = ssh2_sftp($connection);
+
+		$stream = fopen("ssh2.sftp://$sftp/testsFTP.txt", "w+");
+		$data = file_get_contents("ftpTest.txt");
+
+		fwrite($stream, $data);
+		fclose($stream);
+	}
+
+	sftpTransfer();
 ?>
